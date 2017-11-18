fb5d9ad69dd91193da54cbca433b3e066d10429e
Vitor Roriz <v.r.roriz@gmail.com>
2017-11-18 21:45:51 +0100

    implementation of nand gate module

--- nand.h
+++ nand.h
@@ -4,5 +4,21 @@
 #include "systemc.h"
 
 // TODO: Insert your code here
+SC_MODULE (nand)
+{
+	sc_in<bool>	A;
+	sc_in<bool>	B;
+	sc_out<bool>	Z;
+	
+	void do_nand()
+	{
+		Z.write(!(A.read() && B.read()));
+	}
 
+	SC_CTOR(nand)
+	{
+		SC_METHOD(do_nand);
+		sensitive << A << B;
+	}
+};
 #endif
