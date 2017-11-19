16da72a4cbc2d10cc38e90c93af649304f889eda
Vitor Roriz <v.r.roriz@gmail.com>
2017-11-19 02:01:45 +0100

    Adding waveform tracing

--- main.cpp
+++ main.cpp
@@ -5,24 +5,34 @@
 
 int sc_main(int argc, char* argv[])
 {
-    sc_signal<bool> sigA, sigB, sigZ;
+	sc_signal<bool> sigA, sigB, sigZ;
 
-    stim Stim1("Stimulus");
-    Stim1.A(sigA);
-    Stim1.B(sigB);
+	stim Stim1("Stimulus");
+	Stim1.A(sigA);
+	Stim1.B(sigB);
 
-    exor DUT("exor");
-    DUT.A(sigA);
-    DUT.B(sigB);
-    DUT.Z(sigZ);
+	exor DUT("exor");
+	DUT.A(sigA);
+	DUT.B(sigB);
+	DUT.Z(sigZ);
 
-    Monitor mon("Monitor");
-    mon.A(sigA);
-    mon.B(sigB);
-    mon.Z(sigZ);
+	Monitor mon("Monitor");
+	mon.A(sigA);
+	mon.B(sigB);
+	mon.Z(sigZ);
 
-    sc_start();  // run forever
+/* Adding Waveform tracing */
+	sc_trace_file* Tf;
+	Tf = sc_create_vcd_trace_file("traces");
+//	((vcd_trace_file*)Tf)->sc_set_vcd_time_unit(-9);
 
-    return 0;
+	sc_trace(Tf, sigA, "A");
+	sc_trace(Tf, sigB, "B");
+	sc_trace(Tf, sigZ, "Z");
+
+	sc_start();  // run forever
+	sc_close_vcd_trace_file(Tf);
+
+	return 0;
 
 }
