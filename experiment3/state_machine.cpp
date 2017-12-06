a6baaffcb3601540e71ad6eec9c812bb105a1f48
Vitor Roriz <v.r.roriz@gmail.com>
2017-12-06 17:19:25 +0100

    Defining and implemeneting state machine

--- experiment3/state_machine.cpp
+++ experiment3/state_machine.cpp
@@ -1,4 +1,81 @@
 #include "state_machine.h"
 #include <unistd.h>
+#include <iostream>
 
 // TODO
+void stateMachine::process()
+{
+	switch(state)
+	{
+    case Start:
+		switch(input)
+		{
+		case 'A':
+		case 'C':
+		case 'T':
+			state = Start;
+			break;
+		case 'G':
+			state = G;
+            break;
+		}
+		break;
+	case G:
+		switch(input)
+		{
+		case 'G':
+			state = G;
+			break;
+		case 'C':
+		case 'T':
+			state = Start;
+			break;
+		case 'A':
+			state = GA;
+			break;
+		}
+		break;
+	case GA:
+		switch(input)
+		{
+		case 'C':
+		case 'T':
+			state = Start;
+			break;
+		case 'G':
+			state = G;
+			break;
+		case 'A':
+			state = GAA;
+			break;
+		}
+		break;
+	case GAA:
+		switch(input)
+		{
+		case 'A':
+		case 'C':
+		case 'T':
+			state = Start;
+			break;
+		case 'G':
+			state = GAAG;
+            std::cout << "Sequence GAAG detected" << std::endl;
+			break;
+		}
+		break;
+	case GAAG:
+		switch(input)
+		{
+		case 'A':
+		case 'C':
+		case 'T':
+			state = Start;
+			break;
+		case 'G':
+			state = G;
+			break;
+		}
+		break;
+	}
+}
--- experiment3/state_machine.h
+++ experiment3/state_machine.h
@@ -4,7 +4,24 @@
 #include <systemc.h>
 
 // TODO
-
+SC_MODULE(stateMachine)
+{
+public:
+	sc_in<bool> clk;
+	sc_in<char> input;
+    SC_HAS_PROCESS(stateMachine);
+    stateMachine(const sc_module_name &name) : sc_module(name)
+	{
+        state = Start;
+		SC_METHOD(process);
+		sensitive << clk.pos();
+		dont_initialize();
+	}
+private:
+	enum base {Start,G,GA,GAA,GAAG};
+	base state;
+	void process();
+};
 #endif // FSM_H
 
 
