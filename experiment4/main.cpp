9e5e228a17fdd73acff1daf8956eea78819e613c
Vitor Roriz <v.r.roriz@gmail.com>
2017-12-12 10:39:17 +0100

    Base code for Experiment4

---
+++ experiment4/main.cpp
@@ -0,0 +1,20 @@
+#include <systemc.h>
+
+// Place Interface:
+// TODO
+
+// Place Channel:
+// TODO
+
+// Transition:
+// TODO
+
+// TOPLEVEL
+
+int sc_main(int argc, char* argv[])
+{
+    // TODO
+
+    sc_start();
+    return 0;
+}
