4254a4c8e3e83f5be130b5822e31e4ec15ce5f5d
Vitor Roriz <v.r.roriz@gmail.com>
2017-12-13 16:47:36 +0100

    Making an Example of a single memory bank

--- experiment4/main.cpp
+++ experiment4/main.cpp
@@ -5,44 +5,48 @@
 #include "place.cpp"
 
 
-
-
 // TOPLEVEL
 SC_MODULE(toplevel)
 {
     public:
-    transition<1,2> t1;
-    transition<2,1> t2;
-    transition<1,1> t3;
-    Place<1,1> p1, p2, p3, p4;
-    SC_CTOR(toplevel) : t1("t1"), t2("t2"), t3("t3"),
-        p1(1), p2(0), p3(0), p4(0)
+    transition<1,1> ACT;
+    transition<1,1> RD;
+    transition<1,1> PRE;
+    transition<1,1> WR;
+    Place<1,1> IDLE, ACTIVE;
+    SC_CTOR(toplevel) : ACT("ACT"), RD("RD"), PRE("PRE"), WR("WR"),
+       IDLE(1), ACTIVE(0)
     {
         SC_THREAD(process);
-    t1.in.bind(p1);
-    t1.out.bind(p2);
-    t1.out.bind(p3);
-    t2.in.bind(p2);
-    t2.in.bind(p4);
-    t2.out.bind(p1);
-    t3.in.bind(p3);
-    t3.out.bind(p4);
-
+        ACT.in.bind(IDLE);
+        ACT.out.bind(ACTIVE);
+        RD.in.bind(ACTIVE);
+        RD.out.bind(ACTIVE);
+        PRE.in.bind(ACTIVE);
+        PRE.out.bind(IDLE);
+        WR.in.bind(ACTIVE);
+        WR.out.bind(ACTIVE);
     }
     void process() {
         while(true) {
             wait(10,SC_NS);
-            t1.fire();
+            ACT.fire();
             wait(10,SC_NS);
-            t2.fire();
+            ACT.fire();
             wait(10,SC_NS);
-            t3.fire();
+            RD.fire();
             wait(10,SC_NS);
-            t2.fire();
+            WR.fire();
+            wait(10,SC_NS);
+            PRE.fire();
+            wait(10,SC_NS);
+            ACT.fire();
             sc_stop();
+
         }
     }
 };
+
 
 int sc_main(int argc, char* argv[])
 {
